fb4e16cc310e2576d7b3d47c8ad42df8cb3e4561
Luca Baldini <luca.baldini@pi.infn.it>
2026-03-23 14:51:17 +0100

    Copyright notice updated.

--- src/easely/process.py
+++ src/easely/process.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021, luca.baldini@pi.infn.it
+# Copyright (C) 2021--2026, the easely team.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
