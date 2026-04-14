d5e7162b8b55f33e6fad5361933110dca4b65dcf
Luca Baldini <luca.baldini@pi.infn.it>
2026-04-14 06:47:39 +0200

    Initial import.

---
+++ src/easely/program2.py
@@ -0,0 +1,194 @@
+# Copyright (C) 2022--2026, the easely team.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""Basic description of the conference program.
+"""
+
+from dataclasses import dataclass, field
+from datetime import date, datetime
+
+import pandas as pd
+
+from . import schema
+from .logging_ import logger
+from .paths import WorkspaceLayout, sanitize_file_path
+from .typing_ import PathLike
+
+
+@dataclass(frozen=True)
+class Presenter:
+
+    """Presenter descriptor.
+
+    Arguments
+    ---------
+    first_name : str
+        The presenter first name (including middle name initials where appropriate).
+
+    last_name : str
+        The presenter last name.
+
+    affiliation : str
+        The presenter affiliation.
+    """
+
+    first_name: str
+    last_name: str
+    affiliation: str
+
+    def full_name(self) -> str:
+        """Return the presenter full name.
+        """
+        return f'{self.first_name} {self.last_name}'
+
+    def short_affiliation(self, max_chars=25) -> str:
+        """Return a shortened version of the affiliation, trimmed to a fixed maximum
+        number of characters if too long.
+        """
+        if len(self.affiliation) <= max_chars:
+            return self.affiliation.ljust(max_chars)
+        return f'{self.affiliation[:max_chars - 3]}...'
+
+    def __str__(self) -> str:
+        """String formatting.
+        """
+        return f'{self.full_name()} ({self.affiliation})'
+
+
+@dataclass
+class Poster:
+
+    """Poster contribution descriptor.
+
+    Arguments
+    ---------
+    friendly_id : int
+        The unique identifier of the contribution in indico.
+
+    screen_id : int
+        The identifier of the screen the contribution needs to be projected on.
+
+    title : str
+        The contribution title.
+
+    presenter : Presenter instance
+        The contribution presenter.
+    """
+
+    friendly_id: int
+    screen_id: int
+    title: str
+    presenter: Presenter
+
+    @classmethod
+    def from_dataframe_row(cls, row):
+        """Create a Poster object from a dataframe row.
+        """
+        pass
+
+    def short_title(self, max_chars=40):
+        """Return a shortened version of the title, trimmed to a fixed maximum
+        number of characters if too long.
+        """
+        if len(self.title) <= max_chars:
+            return self.title.ljust(max_chars)
+        return f'{self.title[:max_chars - 3]}...'
+
+
+@dataclass
+class Session:
+
+    """Session descriptor.
+    """
+
+    id: int
+    title: str
+    start_datetime: datetime
+    end_datetime: datetime
+    contributions: list[Poster] = field(default_factory=list)
+
+    def __post_init__(self):
+        """Post-initialization processing.
+        """
+        self.start_datetime = Program.parse_datetime(self.start_datetime)
+        self.end_datetime = Program.parse_datetime(self.end_datetime)
+
+    @classmethod
+    def from_dataframe_row(cls, row: pd.core.series.Series) -> "Session":
+        """Create a Poster object from a dataframe row.
+        """
+        return cls(*row)
+
+
+class Program:
+
+    """Conference program descriptor.
+
+    Arguments
+    ---------
+    file_path : PathLike
+        The path to the program excel file.
+    """
+
+    def __init__(self, file_path: PathLike) -> None:
+        """Initialize the program from an excel configuration file.
+        """
+        file_path = sanitize_file_path(file_path, suffix='.xlsx', check_exists=True)
+        self.root_dir = file_path.parent
+        logger.info(f"Loading program data from {file_path}...")
+        # Read the first worksheet, with the conference metadata.
+        schema_ = schema.conference_schema()
+        logger.debug(f"Reading worksheet {schema_.name}...")
+        key_col, value_col = schema_.col_headers()
+        df = pd.read_excel(file_path, sheet_name=schema_.name, header=0)
+        self._validate_sheet(df, schema_)
+        metadata = df.dropna(subset=[key_col]).set_index(key_col)[value_col].to_dict()
+        self.conference_name = metadata['conference_name']
+        self.location = metadata['location']
+        self.start_date = self.parse_date(metadata['start_date'])
+        self.end_date = self.parse_date(metadata['end_date'])
+        logger.debug(f"{self.conference_name}, {self.location}, {self.start_date}--{self.end_date}.")
+        # Read the program worksheet, with the list of sessions.
+        schema_ = schema.program_schema()
+        logger.debug(f"Reading worksheet {schema_.name}...")
+        df = pd.read_excel(file_path, sheet_name=schema_.name, header=0)
+        self._validate_sheet(df, schema_)
+        for _, row in df.iterrows():
+            session = Session.from_dataframe_row(row)
+            print(session)
+
+    @staticmethod
+    def _validate_sheet(df: pd.DataFrame, schema_: schema.SheetSchema) -> None:
+        """Validate a dataframe against the provided schema, raising an exception if the
+        validation fails.
+        """
+        if tuple(df.columns) != schema_.col_headers():
+            raise ValueError(
+                f"Invalid columns in '{schema_.name}'. "
+                f"Expected {schema_.col_headers()}, got {tuple(df.columns)}"
+            )
+
+    @staticmethod
+    def parse_datetime(datetime_str: str) -> date:
+        """Parse a datetime string in the proper schema format and return a datetime object.
+        """
+        return datetime.strptime(datetime_str, schema.DATETIME_FORMAT)
+
+    @staticmethod
+    def parse_date(date_str: str) -> date:
+        """Parse a date string in the proper schema format and return a date object.
+        """
+        return datetime.strptime(date_str, schema.DATE_FORMAT).date()
